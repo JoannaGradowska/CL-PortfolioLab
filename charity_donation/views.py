401ea28d7a91cf2ed2c4440d6882c6f39f47364f
Joanna Gradowska <joanna.gradowska@icloud.com>
2020-01-08 06:24:56 +0100

    Initial commit

---
+++ charity_donation/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic.base import View
+
+
+class LandingPage(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'form.html')
