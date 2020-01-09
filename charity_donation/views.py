3e0d3b6c0b76b3cdc4ec330a2073c21d9920b7f4
Joanna Gradowska <joanna.gradowska@icloud.com>
2020-01-09 06:58:36 +0100

    dynamic bags count

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
+from charity_donation.models import Donation
+
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        donations = Donation.objects.all()
+        bags = 0
+        for bag in donations:
+            bags += bag.quantity
+        return render(request, 'index.html', context={
+            "bags": bags
+        })
 
 
 class AddDonation(View):
