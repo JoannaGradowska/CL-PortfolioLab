71ec7ed328627241a64fdf57b2551ce85d85b3fc
Joanna Gradowska <joanna.gradowska@icloud.com>
2020-01-09 05:46:42 +0100

    category and institution models

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    INSTITUTION_TYPES = (
+        (1, "foundation"),
+        (2, "non-governmental organization"),
+        (3, "local charity collection")
+    )
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    type = models.IntegerField(choices=INSTITUTION_TYPES, default=1)
+    categories = models.ManyToManyField(Category)
